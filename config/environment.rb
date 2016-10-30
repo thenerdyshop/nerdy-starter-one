8f4af6be486a6179b4e1d985dc576ab94d0b26c1
thecookieorg <marko@thecookie.org>
2016-10-30 13:00:11 -0700

    sendgrid added, user_notifier mailer job added

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.default_url_options[:host] = "lvh.me:3000" # development
+#ActionMailer::Base.default_url_options[:host] = "your_domain_name.com" # production
+ActionMailer::Base.smtp_settings = {
+	:address        => 'smtp.sendgrid.net',
+	:port           => '587',
+	:authentication => :plain,
+	:user_name      => ENV['sendgrid_username'],
+	:password       => ENV['sendgrid_password'],
+	:domain         => 'heroku.com',
+	:enable_starttls_auto => true
+}
